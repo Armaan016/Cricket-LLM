b6f30c710b179c85dd343fe8e2329cd547d2b841
Armaan <armaanuddin0016@gmail.com>
2024-06-21 11:44:28 +0530

    Added Videos section in Player Profiles

---
+++ Datasets/CreateEmbeddings.py
@@ -0,0 +1,31 @@
+import json
+import spacy
+from sentence_transformers import SentenceTransformer
+
+nlp = spacy.load("en_core_web_sm")
+
+model = SentenceTransformer("all-MiniLM-L6-v2").to('cuda')
+
+with open(r'C:\Users\raufu\Desktop\Cricket LLM\cricket\Datasets\combined_questions_final.json', 'r') as f:
+    data = json.load(f)
+
+num_sentence_chunk_size = 5
+embeddings = []
+
+for item in data:
+    context = item['context']
+    doc = nlp(context)
+    sentences = [sent.text.strip() for sent in doc.sents]
+
+    sentence_chunks = [sentences[i:i + num_sentence_chunk_size] for i in range(0, len(sentences), num_sentence_chunk_size)]
+
+    context_embedding = model.encode(context) 
+    chunk_text = ' '.join(sentence_chunks)  
+
+    embeddings.append({
+        "context_chunk": context,
+        "context_embedding": context_embedding.tolist(),
+    })
+
+with open('embeddings.json', 'w') as f:
+    json.dump(embeddings, f, indent=4)
